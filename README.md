17c8ddddde1520b286ec60f39482d3db26642371
Austin <austinrmiller1991@gmail.com>
2018-03-16 12:05:45 -0700

    Adds working slack bot handler

---
+++ README.md
@@ -0,0 +1,5 @@
+#Ping Slack Bot
+
+This is an ultra simple slack bot that checks how long it takes a website to respond.
+
+It is meant to run serverless using AWS [API Gateway](https://aws.amazon.com/api-gateway/) and [Lambda](https://aws.amazon.com/lambda/). I use [Up](https://github.com/apex/up) to make my life easier.
---
+++ app.js
@@ -0,0 +1,45 @@
+const { PORT = 3000, TOKEN } = process.env;
+const q = require('q');
+const express = require('express');
+const bodyParser = require('body-parser');
+const restler = require('restler');
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.post("/", function(req, res, next){
+  //https://api.slack.com/slash-commands#validating_the_command
+  if(TOKEN && token !== TOKEN){
+    res.status(403).send("Please go away!");
+    return;
+  }
+
+  var deferred = q.defer();
+  var url = req.body.text;
+  if(url){
+    var start = Date.now();
+    restler.get(url, {timeout: 5000})
+      .on("complete", function(result, response){
+        if (Math.floor(response.statusCode/100) >= 4) {
+          deferred.resolve(`${url} errored ${response.statusCode}`);
+        } else {
+          var end = Date.now();
+          deferred.resolve(`${url} (${response.statusCode}) took ${end - start}ms`);
+        }
+      })
+      .on('timeout', function(ms){
+        deferred.resolve(`${url} timed out after ${ms}ms`);
+      });
+  } else {
+    deferred.resolve("Hi! I am Ping Bot! What url should I ping?");
+  }
+
+  deferred.promise.then(function(text){
+    res.status(200).json({ text: text });
+  });
+});
+
+const server = app.listen(PORT, function(){
+  console.log(`Server listening on port ${PORT}`);
+});
